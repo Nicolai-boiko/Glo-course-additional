2d48caee9de212d9e111fe659e6ae88e5884868f
Nicolai <72386426+Nicolai-boiko@users.noreply.github.com>
2021-04-14 18:14:17 +0300

    init: start task

---
+++ Lesson12/index.js
@@ -0,0 +1,59 @@
+const todoControl = document.querySelector('.todo-control');
+const headerInput = document.querySelector('.header-input');
+const todoList = document.querySelector('.todo-list');
+const todoCompleted = document.querySelector('.todo-completed');
+
+let todoData = JSON.parse(localStorage.getItem('todoItems')) || [];
+
+const render = function () {
+    todoList.textContent = '';
+    todoCompleted.textContent = '';
+
+    todoData.forEach(item => {
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+        li.innerHTML = `<span class="text-todo">${item.value}</span>
+			<div class="todo-buttons">
+				<button class="todo-remove"></button>
+				<button class="todo-complete"></button>
+			</div>
+        `;
+
+        if (item.completed) {
+            todoCompleted.append(li);
+        } else if (item.value.trim() === ''){
+            return;
+        } else {
+            todoList.append(li);
+            headerInput.value = '';
+        };
+
+        const btnTodoComplete = li.querySelector('.todo-complete');
+        btnTodoComplete.addEventListener('click', () => {
+            item.completed = !item.completed;
+            render();
+        });
+        const btnTodoRemove = li.querySelector('.todo-remove');;
+        btnTodoRemove.addEventListener('click', () => {
+            let todoDataWithDeleted = todoData.filter(data => data.value !== item.value);
+            todoData = todoDataWithDeleted.slice();
+            localStorage.setItem('todoItems', JSON.stringify(todoData));
+            li.remove();
+        });
+    });
+};
+
+todoControl.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const newTodo = {
+        value: headerInput.value,
+        completed: false,
+    };
+    todoData.push(newTodo);
+    localStorage.setItem('todoItems', JSON.stringify(todoData));
+    render();
+});
+
+
+render();
